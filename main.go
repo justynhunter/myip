d6cb06c599cec07169ccca518d8f7c97ff4e6a1e
Justyn Hunter <cjhunter@me.com>
2024-07-23 00:06:58 -0500

    rename variable

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	res, err := http.Get("https://api.ipify.org")
+	response, err := http.Get("https://api.ipify.org")
 	if err != nil {
 		os.Stderr.Write([]byte("error contacting api"))
 	}
 
-	ip, err := io.ReadAll(res.Body)
+	ip, err := io.ReadAll(response.Body)
 	if err != nil {
 		os.Stderr.Write([]byte("error parsing response"))
 	}
